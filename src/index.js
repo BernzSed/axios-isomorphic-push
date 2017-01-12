c0892fa753749f1f2af02ccfa84ebf9838c7174e
Bernie Stern <bernzs@gmail.com>
2017-01-11 23:09:24 -0500

    use interceptors instead of wrapping axios

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 import axios from 'axios';
+import url from 'url';
 
 const pushableMethods = ['GET']; // TODO can I push_promise HEAD?
 
-// for request that contain no data (GET, HEAD, DELETE)
-function getRequestConfigWithoutData(arg1, arg2) {
-  if (typeof arg1 === 'string') {
-    const config = Object.assign({}, arg2);
-    config.url = arg1;
-    config.method = config.method || 'GET';
-    return config;
-  } else {
-    return arg1;
-  }
+function canPush(requestUrl, config) {
+  // TODO make sure page request was made in http/2
+  return config.method.toUpperCase() in pushableMethods;
+  // TODO also check domain
+  // TODO don't push the same thing multiple times
 }
-// for requests that contain data (POST, PUT)
-function getRequestConfigWithData(arg1, arg2, arg3) {
-  if (typeof arg1 === 'string') {
-    const config = Object.assign({}, arg3);
-    config.url = arg1;
-    config.method = config.method || 'POST'; // hackish solution, but it works (for now)
-    return config;
-  } else {
-    return arg2 || arg1;
-  }
-}
+
+// TODO should also give the option to disable all push requests and
+//    instead just make requests as normal (for pre-rendering the html)
+//    or ignore all requests (for after the response has been sent).
 
 /**
  * Wraps axios in something that will monitor requests/responses and
  * will issue push requests
- * @param {targetAxios} instance of axios to wrap
- * @param {req} The request to the server (that will return the webpage).
- * @return {string}
+ * @param {pageResponse} The response from the currently running server.
+ * @param {axiosInstance} instance of axios to wrap
+ * @return {object} return an axios instance that will issue push requests automatically.
  */
-export function wrapAxios(targetAxios = axios, req) {
-  // Wish I could use Proxy! But Babel can't polyfill it :(
+export default function prepareAxios(pageResponse, axiosInstance = null) {
+  const targetAxios = (
+      axiosInstance && axiosInstance.create && axiosInstance.create()
+    ) || axiosInstance || axios.create();
 
   if (global.window) {
     // don't wrap it if on client side
@@ -41,72 +32,56 @@ export function wrapAxios(targetAxios = axios, req) {
   }
 
 
-  // can I use axios interceptors instead?
-  // https://github.com/mzabriskie/axios/blob/master/test/specs/interceptors.spec.js
-  // TODO yeah, I should probably be doing that.
-  // ------------------------------------------------------------------------
-  // TODO https://github.com/mzabriskie/axios/#interceptors
-  // ------------------------------------------------------------------------
-  // but can I force it to return a custom promise?
-  //    The interceptor can probably return a fake promise. Hopefully that won't leak memory.
-  //  axios.interceptors.request.use(function(config){ global.console.log('request ', arguments); return new Promise(function(){}) });
-  //  axios('http://www.google.com').then((response) => global.console.log('response'), (error) => global.console.log('promise rejected', error));
-  // Still need a way for redux actions to tell difference between requests from client & server,
-  //      since server side request must be repeated on client,
-  //      so spinner should be displayed but other requests should not be ignored.
-  //      (Have a "promised" and "loading" action and state)
-  // if targetAxios is not an instance, will probably have to call targetAxios.create()
+  // https://github.com/mzabriskie/axios/#interceptors
+  targetAxios.interceptors.request.use(function requestInterceptor(config) {
 
+    const requestUrl = url.parse(config.url);
 
-  function axiosWrapper(...params) {
-    const config = getRequestConfigWithoutData(...params);
-    const method = ((config && config.method) || 'GET').toUpperCase();
-    // if canPush()
-    if (method in pushableMethods) { // TODO move to a canPush() function to keep this code cleaner
-      // TODO:
-      // On server side (assumed).
-      // if we can issue a push_promise,
-      //    Make push_promise and perform the request with ...params.
-      //    When request completes, push response headers & body to client.
-      // else
-      //    No push promise; respond with a dummy promise that never completes/fails
+    if (canPush(requestUrl, config)) {
+      // issue a push promise, with correct authority, path, and headers.
+      // http/2 pseudo headers: :method, :path, :scheme, :authority
+      const requestHeaders = Object.assign({}, config.headers, {
+        ':authority': requestUrl.host
+      }) // TODO exclude unneeded headers like user-agent
+      const pushStream = pageResponse.push(requestUrl.path, {
+        method: config.method,
+        request: requestHeaders
+      }, function pushCallback(err, duplexStream) {
+        // TODO cancel request if something went wrong.
+      });
+      pushStream.on('error', function onPushError() {
+        // TODO cancel request if something went wrong.
+      });
+      const newConfig = Object.assign({}, config, {
+        isomorphicPushStream: pushStream
+      });
+      return newConfig;
     } else {
-      // TODO other stuff
+      // return an empty promise
+      return new Promise(() => {});
     }
-    // TODO all that stuff should be in a reusable function
 
-    // TODO should return promise that contains properties indicating if
-    //   - running on server (because will have to re-do request on client)
-    //   - no request was actually made
-    //  * those should be falsy on client side to mirror unwrapped axios's promises
+  });
+  // undocumented axios feature: use() takes second argument to handle errors
+  targetAxios.interceptors.response.use(function responseInterceptor(response) {
+    // response = { status, statusText, headers, config, request, data }
+    // response.config = { adapter, transformRequest, transformResponse,
+    //    timeout, xsrfCookieName, xsrfHeaderName, maxContentLength,
+    //    validateStatus, headers, method, url, data }
+    const config = response.config;
+    if (config.isomorphicPushStream) {
+      // TODO response headers and status code
+      // sendHeaders(response.headers, callback) // nope, that doesn't work...
+      // Looks like spdy sends the headers & status code immediately:
+      // https://github.com/indutny/spdy-transport/blob/0bc70336508388ff5d111fd5027d3c31a56c7875/lib/spdy-transport/protocol/http2/framer.js#L344
+      config.isomorphicPushStream.end(response.data);
+    }
+  }, function responseRejectedInterceptor(error) {
+    // error = { code, errno, syscall, hostname, host, port, config, response }
+    const config = error.config;
+    // TODO response failed; cancel the push_promise
+  });
 
-    targetAxios(...params);
-  }
 
-  axiosWrapper.request = function () {}; // TODO
-  axiosWrapper.get = function () {}; // TODO
-  axiosWrapper.delete = function () {}; // TODO
-  axiosWrapper.head = function () {}; // TODO
-  axiosWrapper.post = function () {}; // TODO
-  axiosWrapper.put = function () {}; // TODO
-  axiosWrapper.patch = function () {}; // TODO
-
-  // others
-  axiosWrapper.all = targetAxios.all;
-  axiosWrapper.spread = targetAxios.spread;
-  axiosWrapper.create = create;
-
-  axiosWrapper.targetAxios = targetAxios;
-
-  return axiosWrapper;
+  return targetAxios;
 }
-
-export function create(defaults) {
-  // TODO remember defaults.baseUrl and defaults.headers; these will be needed in the push_promise
-  return wrapAxios(axios.create(defaults));
-}
-
-export default wrapAxios();
-
-// TODO do I want to scrap this whole thing and just mock the fetch api instead?
-// https://www.npmjs.com/package/isomorphic-fetch
